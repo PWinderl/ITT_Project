391a722ad5b060defaba30d84e0a29ccb7d40815
PWinderl <paul.winderl@stud.uni-regensburg.de>
2018-07-04 10:17:32 +0200

    Connection to Wiimotes

--- system_demo.py
+++ system_demo.py
@@ -2,6 +2,7 @@
 from PyQt5 import QtWidgets
 import pygame, sys, time
 from pygame.locals import *
+# import wiimote
 
 
 # https://stackoverflow.com/questions/34668981/pygame-unable-to-open-mp3-file
@@ -26,38 +27,46 @@ class MusicPlayer:
 class SetupWidget(QtWidgets.QWidget):
     def __init__(self):
         super(SetupWidget, self).__init__()
-        self.bt_ct_wm_one = QtWidgets.QPushButton(parent=self)
-        self.bt_ct_wm_two = QtWidgets.QPushButton(parent=self)
+        self.bt_start = QtWidgets.QPushButton(parent=self)
         self.init_ui()
 
     def init_ui(self):
         self.setGeometry(0, 0, 1000, 600)
         self.setWindowTitle('Wiimote Hero')
         layout = QtWidgets.QGridLayout(self)
-        layout.setVerticalSpacing(50)
 
-        self.bt_ct_wm_one.setText("Connect first Wiimote")
-        self.bt_ct_wm_one.clicked.connect(self.connect_wiimote)
-        layout.addWidget(self.bt_ct_wm_one, 0, 0)
-
-        self.bt_ct_wm_two.setText("Connect second Wiimote")
-        self.bt_ct_wm_two.clicked.connect(self.connect_wiimote)
-        layout.addWidget(self.bt_ct_wm_two, 0, 1)
+        self.bt_start.setText("Start Game")
+        self.bt_start.clicked.connect(self.start_game)
+        layout.addWidget(self.bt_start, 0, 0)
 
         self.show()
 
-    def connect_wiimote(self, event):
+    def start_game(self, event):
         bluetooth_con = SetupBluetooth()
+
 
 # To connect to Wiimotes
 class SetupBluetooth:
-    def __init__(self):
+    def __init__(self, address_one, address_two):
+        try:
+            self.wm_one = wiimote.connect(address_one)
+            self.wm_two = wiimote.connect(address_two)
+        except BluetoothError:
+            print("no valid bluetooth address")
+            sys.exit()
+            return
+
+
         mp = MusicPlayer()
 
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
     main_window = SetupWidget()
+    if len(sys.argv) == 2:
+        connect_wiimotes = SetupBluetooth(sys.argv[1], sys.argv[2])
+    else:
+        pass
 
     sys.exit(app.exec_())
 
